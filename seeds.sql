967b7638180121b19d7bd119191ede501b80a96d
James Davis <james.jpdavis@gmail.com>
2016-05-09 18:16:25 -0400

    first push

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO burgers (burgerName, devoured, date) VALUES ('Bacon Cheese', FALSE, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burgerName, devoured, date) VALUES ('Vegan', FALSE, CURRENT_TIMESTAMP);
+
+INSERT INTO burgers (burgerName, devoured, date) VALUES ('Heart Attack', FALSE, CURRENT_TIMESTAMP);
