0f0b784ed95ef1c782b77c5560b9d0f0843703b9
James Davis <james.jpdavis@gmail.com>
2016-05-14 01:10:29 -0400

    updates

--- config/orm.js
+++ config/orm.js
@@ -4,25 +4,25 @@ var tableName= 'burger';
 
 
 var orm = {
-    selectAll: function(cb) {
+    burgerAll: function(cb) {
         var s = 'SELECT * FROM ' + tableName + ';';
         connection.query(s, function(err, result) {
             if (err) throw err;
             cb(result);
         });
     },
-    insertOne: function(nameInput, devourInput, cb) {
+    newBurger: function(burger,cb) {
     	var s = 'INSERT INTO ' + tableName + ' (burger_name, devoured) VALUES (?, ?)';
-	  	connection.query(s, [nameInput, devourInput], function(err, result) {
+	  	connection.query(s, [burger, false], function(err, result) {
 			if (err) throw err;
-			cb(result);
+			cb();
 		});
     },
-    updateOne: function( colInput, idInput, cb) {
-    	var s = 'UPDATE ' + tableName + ' SET devoured = ? WHERE id = ?';
-     	connection.query(s, [colInput, idInput], function(err, result) {
+    eatBurger: function( id, cb) {
+    	var s = 'UPDATE ' + tableName + ' SET devoured = 1 WHERE id = ?';
+     	connection.query(s, [id], function(err, result) {
 			if (err) throw err;
-			cb(result);
+			cb();
 		});
     }
 };
---
+++ routes/api-routes.js
@@ -0,0 +1,5 @@
+var path= require('path');
+var orm = require ('./config/orm.js');
+
+
+
